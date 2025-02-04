33ca463bdbefed4a4e9b58b82ac6cbf9b17cbfb2
Geoff Ehrman <gehrman@gmail.com>
2025-02-03 21:13:14 -0500

    Implement upward scanning

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use std::env::current_dir;
+use std::process::exit;
 
 
 fn main() {
+    let needle = ".env";  // TODO: make this a CLI flag
+
     for dir in current_dir().unwrap().ancestors() {
-        println!("Dir is {}", dir.display());
+        let file = dir.join(needle);
+        if file.exists() {
+            println!("{}", file.display());
+            exit(0)
+        }
     }
+    println!(".env file not found");  // TODO: put this behind -v flag
+    exit(1)
 }
