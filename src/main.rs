608c7c53d0789cebfd02f2d550954cbd08ad7271
Geoff Ehrman <gehrman@gmail.com>
2025-02-08 17:06:29 -0500

    Add a CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
+use clap::Parser;
 use std::env::current_dir;
 use std::process::exit;
 
+/// Search up the directory tree for file named TARGET
+#[derive(Parser, Debug)]
+#[command(version, about, long_about)]
+struct Args {
+    /// Optional file name to search for
+    target: Option<String>,
+
+    /// Control the verbosity
+    #[arg(short, default_value_t = false)]
+    verbose: bool,
+}
 
 fn main() {
-    let needle = ".env";  // TODO: make this a CLI flag
+    let args = Args::parse();
+    if args.verbose {
+        println!("Called with {:?}", args);
+    }
+
+    let needle = match args.target {
+        Some(name) => name,
+        None => String::from(".env"),
+    };
 
     for dir in current_dir().unwrap().ancestors() {
-        let file = dir.join(needle);
+        let file = dir.join(needle.clone());
         if file.exists() {
             println!("{}", file.display());
             exit(0)
         }
     }
-    println!(".env file not found");  // TODO: put this behind -v flag
+    if args.verbose {
+        println!("{} not found", needle);
+    }
     exit(1)
 }
